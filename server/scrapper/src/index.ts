a6abab6217c18047a78c282ba94e310a2ac6df90
Carlosdp7 <danielperandrad10@gmail.com>
2024-11-02 16:10:25 -0400

    ref: Refactor scrapper code

--- server/scrapper/src/index.ts
+++ server/scrapper/src/index.ts
@@ -1,9 +1,6 @@
+require("dotenv").config();
 import fs from "fs/promises";
-import { chromium } from "playwright";
-
-function delay(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+import { chromium, Page } from "playwright";
 
 (async () => {
   const browser = await chromium.launch({ headless: false });
@@ -12,32 +9,57 @@ function delay(ms: number) {
 
   await page.goto("https://app.reonomy.com/!/search");
 
-  await delay(100);
+  await delay(1000);
 
-  await page.click('[type="email"]');
-  await page.keyboard.insertText("ernest@earnest3d.com");
-  await page.click('[type="password"]');
-  await page.keyboard.insertText("ernest5650");
-  await page.click(".auth0-label-submit");
+  await signIn(page);
 
   await delay(1000);
 
+  await setFilters(page);
+
+  await searchLocation(page);
+
+  await delay(4000);
+
+  await getBuildingData(page);
+
+  await browser.close();
+})();
+
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function signIn(page: Page) {
+  if (!process.env.EMAIL || !process.env.PASSWORD) {
+    throw new Error("Email and password are required");
+  }
+  await page.click('[type="email"]');
+  await page.keyboard.insertText(process.env.EMAIL);
+  await page.click('[type="password"]');
+  await page.keyboard.insertText(process.env.PASSWORD);
+  await page.click(".auth0-label-submit");
+}
+
+async function setFilters(page: Page) {
   await page.click('[data-testid="Property Type"]');
   await page.locator("text=Shopping Centers & Stores").click();
   await page.locator("text=Restaurants").click();
   await page.locator("text=Commercial General").click();
 
   await page.locator("text=Apply").click();
+}
 
+async function searchLocation(page: Page) {
   await page.click('[id="location-search-input"]');
   await page.keyboard.insertText("Orlando, FL");
 
   await delay(1000);
 
   await page.keyboard.press("Enter");
+}
 
-  await delay(4000);
-
+async function getBuildingData(page: Page) {
   const buildings = page
     .locator("#summary-cards-list-container")
     .locator(">div");
@@ -52,6 +74,4 @@ function delay(ms: number) {
     const text = await building.textContent();
     if (text) await fs.appendFile("./buildings.txt", text);
   }
-
-  await browser.close();
-})();
+}
